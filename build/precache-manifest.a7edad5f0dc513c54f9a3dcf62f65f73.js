150673a3d9ead0e12428b2d74eb0ff2fbfb052ad
Rob <robert.j1450@gmail.com>
2020-02-05 18:11:53 -0500

    github pages

---
+++ build/precache-manifest.a7edad5f0dc513c54f9a3dcf62f65f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de0f6d6f711c813001d73657e0f63da",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "844bd53190892046aa93",
+    "url": "/monsters-rolodex/static/css/main.7626a7e2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "844bd53190892046aa93",
+    "url": "/monsters-rolodex/static/js/main.8ce9ba9f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
