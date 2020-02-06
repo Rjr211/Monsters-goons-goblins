0e2a5e9952588de69e68b990470edf5c75ec3b59
Rob <robert.j1450@gmail.com>
2020-02-05 23:59:57 -0500

    Updates

---
+++ precache-manifest.4f40ba208ff30975082c713a7ac6d67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6224139c18a504e1c9230a9043538d0f",
+    "url": "/Monsters-goons-goblins/index.html"
+  },
+  {
+    "revision": "050168ff1c8a3ed9ee9b",
+    "url": "/Monsters-goons-goblins/static/css/main.7626a7e2.chunk.css"
+  },
+  {
+    "revision": "be3e1783ba71477b0e56",
+    "url": "/Monsters-goons-goblins/static/js/2.9aeb243a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-goons-goblins/static/js/2.9aeb243a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "050168ff1c8a3ed9ee9b",
+    "url": "/Monsters-goons-goblins/static/js/main.b3855f26.chunk.js"
+  },
+  {
+    "revision": "8a74ccef6e241f08227d",
+    "url": "/Monsters-goons-goblins/static/js/runtime-main.8fe4388d.js"
+  }
+]);
